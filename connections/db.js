64f61b03ab20e60b3b3e3fe808654e405fbeb019
Michael Mroz <michaelmroz01111@gmail.com>
2026-07-10 10:16:16 -0300

    add cookie parser dependency

--- connections/db.js
+++ connections/db.js
@@ -2,8 +2,9 @@ import mysql from "mysql2/promise";
 import 'dotenv/config';
 
 export const pool = mysql.createPool({
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "tenanttrails",
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
